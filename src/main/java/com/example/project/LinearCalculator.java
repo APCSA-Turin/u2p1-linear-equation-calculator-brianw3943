cdc0b4d8d41304c7b7ab3075930ba2462d9e2a09
brianw3943 <brianw124@nycstudents.net>
2024-10-22 16:35:55 +0000

    a

--- src/main/java/com/example/project/LinearCalculator.java
+++ src/main/java/com/example/project/LinearCalculator.java
@@ -13,20 +13,30 @@ public class LinearCalculator{
     //For example, "(1,2)" and "(3,4)" would be two parameter values 
     //You will have to parse the string into 4 integers, representing the 2 points.
     public LinearCalculator(String coord1, String coord2){ // <--add 2 string parameters to this constructor
-        x1 = Integer.parseInt(coord1.substring(1, 2));
-        x2 = Integer.parseInt(coord2.substring(1, 2));
-        y1 = Integer.parseInt(coord1.substring(coord2.length()-1));
-        y2 = Integer.parseInt(coord2.substring(coord2.length()-1));
+        int temp = coord1.indexOf(",");
+        x1 = Integer.parseInt(coord1.substring(1, temp));
+        y1 = Integer.parseInt(coord1.substring(temp + 1, coord1.length()-1));
+        temp = coord2.indexOf(",");
+        x2 = Integer.parseInt(coord2.substring(1, temp));
+        y2 = Integer.parseInt(coord2.substring(temp + 1, coord2.length()-1));
     }
 
 
 
     //METHODS
     //getters and setters for the 4 instance variables (8 methods total) 
-    public int getX1(){return x1;}
-    public int getY1(){return y1;}
-    public int getX2(){return x2;}
-    public int getY2(){return y2;}
+    public int getX1(){
+        return x1;
+    }
+    public int getY1(){
+        return y1;
+    }
+    public int getX2(){
+        return x2;
+    }
+    public int getY2(){
+        return y2;
+    }
     public void setX1(int newX1){
         x1 = newX1;
     }
@@ -44,7 +54,7 @@ public class LinearCalculator{
     //distance() -> returns a double. 
     //calculates the distance between the two points to the nearest HUNDREDTH and returns the value.
     public double distance(){
-        double distance = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
+        double distance = Math.sqrt((double) Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         distance = distance * 100;
         distance = Math.round(distance);      // I learned how to round from here: https://stackoverflow.com/questions/8825209/rounding-decimal-points
         distance = distance / 100;
@@ -55,12 +65,15 @@ public class LinearCalculator{
     //if y-int if undefined, should return -999.99
     public double yInt(){
         double yInt;
-        if () {
+        if ((x2 - x1) == 0) {
             yInt = -999.99;
         } else {
-            
+            x2 = x1;
+            y2 = y1;
+            x1 = 0;
+            yInt = y2 - ((double)slope()/x2);
         }
-        return 0.0;
+        return yInt;
     }
 
     //slope() -> returns a double. 
@@ -71,7 +84,7 @@ public class LinearCalculator{
         if ((x2 - x1) == 0) {
             slope = -999.99;
         } else {
-            slope = (y2 - y1) / (x2 - x1);
+            slope = (double)(y2 - y1) / (x2 - x1);
             slope = slope * 100;
             slope = Math.round(slope);
             slope = slope / 100;
@@ -85,10 +98,10 @@ public class LinearCalculator{
     //HINT: You may need other custom methods to decrease the amount of code in the equations() method
     public String equation(){
         String equation;
-        if (slope == -999.99) {
+        if (slope() == -999.99) {
             equation = "undefined";
         } else {
-            equation = "y=" + slope + "x+" + yInt;
+            equation = "y=" + slope() + "x+" + yInt();
         }
         return equation;
     }
@@ -109,14 +122,12 @@ public class LinearCalculator{
     public String printInfo(){
         String str = "The two points are: (" + x1 + "," + y1  + ")";
         str += " and " + "(" + x2 + "," + y2 + ")";
-        str += "\nThe equation of the line between these points is: " ;
-        str += "\nThe slope of this line is: " + slope;
-        str += "\nThe y-intercept of the line is: " + yInt;
-        str += "\nThe distance between the two points is: " + distance;
+        str += "\nThe equation of the line between these points is: " + equation();
+        str += "\nThe slope of this line is: " + slope();
+        str += "\nThe y-intercept of the line is: " + yInt();
+        str += "\nThe distance between the two points is: " + distance();
  
         return str;
     }
-
-
 
 }
---
+++ src/main/java/com/example/project/LinearCalculatorRunner.java
@@ -0,0 +1,10 @@
+package com.example.project;
+
+public class LinearCalculatorRunner {
+    public static void main(String[] args) {
+    //   LinearCalculator c = new LinearCalculator("(0,1)", "(2,3)");
+    //   System.out.println(c.getX1());  
+    LinearCalculator c = new LinearCalculator("(-3,-1)","(4,-3)");
+    c.distance();
+    }
+}
